9a276605fb69b26151016f99f9b3806632e0bb4d
Anthony_256 <101401469+AnthonyByansi@users.noreply.github.com>
2022-10-09 23:48:34 +0300

    Update js functions

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-
+function showLinks() {
+  let links = document.getElementsByClassName('links')[0];
+  
+  if (links.classList.contains('hidden')) {
+    links.classList.remove('hidden');
+  } else {
+    links.classList.toggle('hidden');
+  }
+}
